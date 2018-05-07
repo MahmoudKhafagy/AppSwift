cce6f9c3727311f04ce12b9617535449661ea9ff
jpo <jpo@imac04.local>
2018-05-07 19:12:02 +0200

    Initial Commit

---
+++ dev/ViewController.swift
@@ -0,0 +1,26 @@
+//  ViewController.swift
+//  dev
+//
+//  Created by Arnaud on 01/02/2018.
+//  Copyright © 2018 IIM. All rights reserved.
+
+
+import UIKit
+
+// ajout des protocoles delegate et datasource (cmd. pour aller aux methodes obligatoires)
+class ViewController: UIViewController
+{
+
+    
+    
+    // outlet
+    
+    // actions
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
+
